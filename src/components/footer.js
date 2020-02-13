dba75fbd220d2fb7ca05fcc56ca4e84fd03a6222
Terrence <teisenhower@gmail.com>
2020-02-13 15:05:53 -0500

    Live Version - V1.0!

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,35 +1,76 @@
-import React from 'react'
-import styles from './footer.module.css'
+import React from "react";
+import styles from "./footer.module.css";
+import config from "react-global-configuration";
+import configuration from "../config";
 const date = new Date();
 let currentYear = date.getFullYear();
+const socialLinks = config.get("social");
 
-class Footer extends React.Component{
- render(){
-  return(
-   <div className={styles.footer}>
-    <div className={styles.footerCats}>
-     <div className={styles.footerItem}>
-      <div className={"medium medGrey " + styles.footerItemTitle}>Need Help?</div>
-      <ul className={styles.footerList}>
-       <li><a class={"medGrey " + styles.contactInfo} href="tel:+1 310 384-2540">(310) 384-2540</a></li>
-       <li><a class={"medGrey " + styles.contactInfo}  href="mailto:elliot@eaglesports.biz">Elliot@eaglesports.biz</a></li>
-      </ul>
-     </div>
-     <div className={styles.footerItem}>
-      <div className={"medium medGrey " + styles.footerItemTitle}>Connect With Us</div>
-      <ul className={styles.footerList}>
-       <li><a target="_blank" rel="noopener noreferrer" class={"medGrey " + styles.contactInfo} href="https://www.facebook.com/">Facebook</a></li>
-       <li><a target="_blank" rel="noopener noreferrer" class={"medGrey " + styles.contactInfo} href="https://www.linkedin.com/">LinkedIn</a></li>
-       <li><a target="_blank" rel="noopener noreferrer" class={"medGrey " + styles.contactInfo} href="https://www.yelp.com/">Yelp</a></li>
-      </ul>
-     </div>
-    </div>
-    <div className={styles.copyright}>
-     <small className="medGrey">Copyright &copy; {currentYear} Eagle Sports and Awards. All Rights Reserved</small>
-    </div>
-   </div>
-  )
- }
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className={styles.footer}>
+        <div className={styles.footerCats}>
+          <div className={styles.footerItem}>
+            <div className={"medium medGrey " + styles.footerItemTitle}>
+              Need Help?
+            </div>
+            <ul className={styles.footerList}>
+              <li>
+                <a
+                  class={"medGrey " + styles.contactInfo}
+                  href="tel:+1 310 384-2540"
+                >
+                  (310) 384-2540
+                </a>
+              </li>
+              <li>
+                <a
+                  class={"medGrey " + styles.contactInfo}
+                  href="mailto:elliot@eaglesports.biz"
+                >
+                  Elliot@eaglesports.biz
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.footerItem}>
+            <div className={"medium medGrey " + styles.footerItemTitle}>
+              Connect With Us
+            </div>
+            <ul className={styles.footerList}>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class={"medGrey " + styles.contactInfo}
+                  href={socialLinks[0].facebook}
+                >
+                  Facebook
+                </a>
+              </li>
+              <li>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class={"medGrey " + styles.contactInfo}
+                  href={socialLinks[0].yelp}
+                >
+                  Yelp
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.copyright}>
+          <small className="medGrey">
+            Copyright &copy; {currentYear} Eagle Sports and Awards. All Rights
+            Reserved
+          </small>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Footer
+export default Footer;
--- src/config.js
+++ src/config.js
@@ -1,106 +1,118 @@
 const config = {
+  social: [
+    {
+      yelp:
+        "https://www.yelp.com/biz/eagle-sports-and-awards-company-winnetka-2",
+      facebook: "https://fb.me/EagleSportsAndAwards"
+    }
+  ],
   about: [
     "Vietnam Veteran owned",
-    "Working in public recreation for more than 20 years and raising children, Elliot had first hand understanding of the needs of recreation departs and schools, including PTAs.  So when he retired after over 20 years ago, he decided to provide a different service to the communities around him.",
+    "Working in public recreation for more than 20 years and raising children, Elliot had first hand understanding of the needs of recreation departments and schools, including PTAs.  So when he retired after over 20 years ago, he decided to provide a different service to the communities around him.",
     "As a screen printer, embroidery, trophies &awards and promotional item company in the San Fernando Valley, we provide services to the greater LA area.  Elliot has insight into the needs of groups both large and small.  We have a graphic designer on our staff who can assist with getting your custom design on your chosen piece of apparel, whether T-shirt, sweatshirt, polo shirt, hats, etc. whether screen printed or embroidered.  All of screen printing and embroidery is done locally in the Los Angeles area."
   ],
- us: [
-  {
-    title: "LOCATION",
-    description: "Eagle Sports is conveniently located in sunny Winnetka California, and is accessible by cities all around."
-  },
-  {
-   title: "YOU",
-   description: "It’s all about the customer in our book. We are not happy until you are. We respond to our emails within 12 hours. We take all inquires seriously and try our best to comply with all issues and or questions as fast as possible"
-  },
-  {
-   title: "DESIGN",
-   description: "No design is too difficult for us. Send us an email and we be sure to evaluate your design and guide you through the process. We pay attention to the tiniest amount of detail to ensure both quality and happiness as a customer."
-  },
-  {
-   title: "TURN AROUND",
-   description: "Eagle Sports & Awards prides itself on having the quickest turnarounds, with no extra charge & in most cases 3 days."
-  },
-  {
-   title: "CONNECT",
-   description: "We love to hear feedback. We also love giving advice. Send us an email and we will be more than glad to assist you with your order."
-  },
- ],
- clients: [
-  {
-    title: "BOYS AND GIRLS CLUBS",
-    names: [
-      "Santa Monica",
-    "Mar Vista",
-    "Venice",
-    "East LA",
-    "Tustin",
-    "Santa Clarita",
-   ]
-  },
-  {
-   title: "CITIES",
-   names: [
-     "Culver City",
-     "Paramount",
-     "West Hollywood",
-     "Westchester",
-     "Van Nuys",
-     "Moorpark",
-   ]
-  },
-  {
-   title: "SCHOOLS",
-   names: [
-    "LA Ballona",
-    "Lin Howe",
-    "Farragut",
-    "Beverly Hills",
-    "Temple Akiba",
-    "Pacific Palisades"
-   ]
-  },
-  {
-   title: "NON-PROFIT",
-   names: [
-    "Lupus Foundation",
-    "Hearts For Hope",
-    "Resnick Child Life Center"
-   ]
-  },
-  {
-   title: "OTHERS",
-   names: [
-    "Honeykettle Chicken",
-    "Factor's Famous Deli",
-    "Playboy",
-    "Beverly Hills Tennis",
-    "Paramount Studios"
-   ]
-  },
- ],
- brands: [
-  {
-   title: "Alphabroder",
-   url: "https://www.alphabroder.com/"
-  },
-  {
-   title: "Sanmar",
-   url: "https://www.sanmar.com/"
-  },
-  {
-   title: "A4",
-   url: "https://www.a4.com/"
-  },
-  {
-   title: "NissinCap",
-   url: "https://www.nissincap.com/"
-  },
-  {
-   title: "Ottocap",
-   url: "https://ottocap.com/"
-  },
- ]
-}
+  us: [
+    {
+      title: "LOCATION",
+      description:
+        "Eagle Sports is conveniently located in sunny Winnetka California, and is accessible by cities all around."
+    },
+    {
+      title: "YOU",
+      description:
+        "It’s all about the customer in our book. We are not happy until you are. We respond to our emails within 12 hours. We take all inquires seriously and try our best to comply with all issues and or questions as fast as possible"
+    },
+    {
+      title: "DESIGN",
+      description:
+        "No design is too difficult for us. Send us an email and we be sure to evaluate your design and guide you through the process. We pay attention to the tiniest amount of detail to ensure both quality and happiness as a customer."
+    },
+    {
+      title: "TURN AROUND",
+      description:
+        "Eagle Sports & Awards prides itself on having the quickest turnarounds, with no extra charge & in most cases 3 days."
+    },
+    {
+      title: "CONNECT",
+      description:
+        "We love to hear feedback. We also love giving advice. Send us an email and we will be more than glad to assist you with your order."
+    }
+  ],
+  clients: [
+    {
+      title: "BOYS AND GIRLS CLUBS",
+      names: [
+        "Santa Monica",
+        "Mar Vista",
+        "Venice",
+        "East LA",
+        "Tustin",
+        "Santa Clarita"
+      ]
+    },
+    {
+      title: "CITIES",
+      names: [
+        "Culver City",
+        "Paramount",
+        "West Hollywood",
+        "Westchester",
+        "Van Nuys",
+        "Moorpark"
+      ]
+    },
+    {
+      title: "SCHOOLS",
+      names: [
+        "LA Ballona",
+        "Lin Howe",
+        "Farragut",
+        "Beverly Hills",
+        "Temple Akiba",
+        "Pacific Palisades"
+      ]
+    },
+    {
+      title: "NON-PROFIT",
+      names: [
+        "Lupus Foundation",
+        "Hearts For Hope",
+        "Resnick Child Life Center"
+      ]
+    },
+    {
+      title: "OTHERS",
+      names: [
+        "Honeykettle Chicken",
+        "Factor's Famous Deli",
+        "Playboy",
+        "Beverly Hills Tennis",
+        "Paramount Studios"
+      ]
+    }
+  ],
+  brands: [
+    {
+      title: "Alphabroder",
+      url: "https://www.alphabroder.com/"
+    },
+    {
+      title: "Sanmar",
+      url: "https://www.sanmar.com/"
+    },
+    {
+      title: "A4",
+      url: "https://www.a4.com/"
+    },
+    {
+      title: "NissinCap",
+      url: "https://www.nissincap.com/"
+    },
+    {
+      title: "Ottocap",
+      url: "https://ottocap.com/"
+    }
+  ]
+};
 
 export default config;
